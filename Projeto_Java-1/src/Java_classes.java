4bf0b1820fbefda65cd094d41e20c628dfb3a143
Janderson-Mota <jandersonmota2407@gmail.com>
2025-01-23 18:25:45 -0300

    Atualização 2

--- Projeto_Java-1/src/Java_classes.java
+++ Projeto_Java-1/src/Java_classes.java
@@ -2,9 +2,7 @@ public class Java_classes {
 
     
     public static void main(String[] args) {
-
-        int classe= 10;
-        System.out.println(classe);
+      System.out.println("Óla mundo!");
     }
     
 }
