40c0cf3508f4f2e08d1da88bfeae5f8df4d08e14
Janderson-Mota <jandersonmota2407@gmail.com>
2025-01-24 19:23:08 -0300

    Atualização 5

--- Projeto_Java-1/src/edu/Janderson/classesVariaveis/Java_variaveis_1.java
+++ Projeto_Java-1/src/edu/Janderson/classesVariaveis/Java_variaveis_1.java
@@ -2,10 +2,17 @@ package edu.Janderson.classesVariaveis;
 
 public class Java_variaveis_1 {
 
-    //Primeiro algoritmo.
     public static void main(String[] args) {
-
+ 
+        //Tipos de dados numericos:
         
+        byte idade = 120;
+        short ano = 2021;
+        int cep = 21070333; // Se comecar com zero, talvez tenha que ser outro tipo
+        long cpf = 92874783787L; // Se comecar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1234.45; 
+         
          
 
     }
