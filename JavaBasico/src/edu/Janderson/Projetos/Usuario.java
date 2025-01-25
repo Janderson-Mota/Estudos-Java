7ee3e7e42bf28f9d756e1a9ae322eacef34aba1e
Janderson-Mota <jandersonmota2407@gmail.com>
2025-01-25 17:45:18 -0300

    Atualização 9

--- JavaBasico/src/edu/Janderson/Projetos/Usuario.java
+++ JavaBasico/src/edu/Janderson/Projetos/Usuario.java
@@ -9,26 +9,26 @@ public static void main(String[] args) {
 
           //Status televisão
           smartTv.ligar();{
-                System.out.println("Novo Status -> Tv ligada :" + smartTv.ligada);}
+                System.out.println("Novo Status -> Tv ligada : " + smartTv.ligada);}
 
           smartTv.desligar();{
-                System.out.println("Novo Status -> Tv ligada :" + smartTv.ligada);}
+                System.out.println("Novo Status -> Tv Desligada : " + smartTv.ligada);}
   
           
           //Status Canal
           smartTv.aumentarCanal();{
-               System.out.println("Canal atual :" + smartTv.canal);}
+               System.out.println("Canal aumentado para : " + smartTv.canal);}
           
           smartTv.diminuirCanal();{
-               System.out.println("Canal atual :" + smartTv.canal);}
+               System.out.println("Canal diminuindo para : " + smartTv.canal);}
 
 
           //Status Volume
           smartTv.aumentarVolume();{
-               System.out.println("Volume atual :" + smartTv.volume);}
+               System.out.println("Volume aumentando para : " + smartTv.volume);}
 
                smartTv.diminuirVolume();{
-                    System.out.println("Volume atual :" + smartTv.volume);}
+                    System.out.println("Volume diminuindo para : " + smartTv.volume);}
 
   
   
