48cee355ef4a114338a113d935aa15aa6329b6b4
Janderson-Mota <jandersonmota2407@gmail.com>
2025-02-23 12:08:57 -0300

    atualização 16

--- Projetos_Diversos/src/exercicios_EstruturasCondicionais/calculoImposto.java
+++ Projetos_Diversos/src/exercicios_EstruturasCondicionais/calculoImposto.java
@@ -15,12 +15,27 @@ public class calculoImposto {
     System.out.println("Digite o seu salario para a verificação do imposto:");
     salario = sc.nextDouble();
     
-    if(salario <= 2.000){
+    if(salario <= 2000.00){
+        System.out.print ("Isento");
+    }
+    else if(salario <= 3000){
     	imposto = 8;
         totalImposto =(salario*imposto)/100;
-        System.out.println (totalImposto);
+        System.out.print ("O imposto total é de: R$ " + totalImposto);
     }
-    
+    else if(salario <= 4500){
+    	imposto = 18;
+        totalImposto =(salario*imposto)/100;
+        System.out.print ("O imposto total é de: R$ " + totalImposto);
+    }
+    else if(salario > 4501){
+    	imposto = 28;
+        totalImposto =(salario*imposto)/100;
+        System.out.print ("O imposto total é de: R$ " + totalImposto);
+    }
+    else {
+    	System.out.print ("Isento");
+    }
      
      
      
