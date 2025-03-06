36d311a9ccd3cac05c5a974bfc9eb664a45ea9c3
Janderson-Mota <jandersonmota2407@gmail.com>
2025-03-05 21:53:46 -0300

    Atualização 21

---
+++ ProjetoLoja/src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		Product produto = new Product();
+		System.out.println("Digite os dados do produto:");
+		
+		System.out.print("Name: ");
+		produto.nome = sc.next();
+		
+		System.out.print("Preço: ");
+		produto.preco = sc.nextDouble();
+		
+		System.out.print("Quantidade no estoque: ");
+		produto.quantidade = sc.nextInt();
+		
+		System.out.println("");
+		System.out.println("Os dados do produto : "+produto);
+		
+		System.out.println("");
+		System.out.print("Entre com o número para ser acrescentado no estoque: ");
+		int quantidade = sc.nextInt();
+		produto.adicionarProdutos(quantidade);
+		
+		System.out.println("");
+		System.out.print("Produto atualizado: "+ produto);
+		
+		System.out.println("");
+		System.out.print("Entre com o número para ser removidos no estoque: ");
+		quantidade = sc.nextInt();
+		produto.removerProdutos(quantidade);
+		
+		System.out.println("");
+		System.out.print("Produto atualizado: "+ produto);
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
