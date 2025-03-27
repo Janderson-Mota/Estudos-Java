308ef2186a23ef26f4f90340b93018a82ff55ede
Janderson-Mota <jandersonmota2407@gmail.com>
2025-03-26 22:49:04 -0300

    atualização 26

---
+++ ProjetoBiblioteca/src/sistemaUsuario/usuario.java
@@ -0,0 +1,18 @@
+package sistemaUsuario;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class usuario {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		sc.close();
+	}
+
+}
